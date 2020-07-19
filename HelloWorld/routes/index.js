48de6332a7167dbf011ba8d186b1ad4a036935f6
unknown <profazpcb@gmail.com>
2020-07-19 12:27:17 -0400

    Adding rest of the files

---
+++ HelloWorld/routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const SerialPort = require('serialport');
+//const Readline = require('@serialport/parser-readline')
+const port = new SerialPort('COM5', { baudRate: 115200 })
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'wasdwasd' });
+});
+
+router.post('/submit-teacher-data', function (req, res) {
+  var name = req.body.firstName + ' ' + req.body.lastName;
+
+  //res.send(name + ' Submitted Successfully!');
+
+port.write('leftup')
+
+console.log(name + 'done!!!');
+res.redirect('/');
+
+});
+router.post('/submit-student-data', function (req, res) {
+  var name = req.body.firstName + ' ' + req.body.lastName;
+
+  //res.send(name + ' Submitted Successfully!');
+
+  port.write('leftdown')
+
+  console.log(name + 'FINISHED');
+res.redirect('/');a 1234
+module.exports = router;
