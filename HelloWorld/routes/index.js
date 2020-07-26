10143d944e9e8eb0695c9353e12928d182e463f7
unknown <profazpcb@gmail.com>
2020-07-26 11:52:30 -0400

    Fixed errors

--- HelloWorld/routes/index.js
+++ HelloWorld/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const SerialPort = require('serialport');
 //const Readline = require('@serialport/parser-readline')
-const port = new SerialPort('COM5', { baudRate: 115200 })
+//const port = new SerialPort('COM5', { baudRate: 115200 })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,19 +15,22 @@ router.post('/submit-teacher-data', function (req, res) {
 
   //res.send(name + ' Submitted Successfully!');
 
-port.write('leftup')
+//port.write('leftup')
 
-console.log(name + 'done!!!');
-res.redirect('/');
+  console.log(name + 'done!!!');
+  res.redirect('/');
 
 });
-router.post('/submit-student-data', function (req, res) {
+
+router.post('/submit-student-data', function (req, resZxv, next  ) {
   var name = req.body.firstName + ' ' + req.body.lastName;
 
   //res.send(name + ' Submitted Successfully!');
 
-  port.write('leftdown')
+  //port.write('leftdown')
 
   console.log(name + 'FINISHED');
-res.redirect('/');a 1234
-module.exports = router;
+  res.redirect('/');
+});
+  module.exports = router;
+
